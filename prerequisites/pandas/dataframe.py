a75b2b395936c0ec7ed568261ecd53a44c02a753
Shivanshu Agnihotri <shivanshuagnihotri27@gmail.com>
2020-05-02 12:16:44 +0530

    Add files via upload

---
+++ prerequisites/pandas/dataframe.py
@@ -0,0 +1,55 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "   Day  Visitors\n",
+      "0    1       100\n",
+      "1    2       200\n",
+      "2    3        50\n"
+     ]
+    }
+   ],
+   "source": [
+    "import pandas as pd\n",
+    "d={'Day':[1,2,3], 'Visitors':[100,200,50]}\n",
+    "df=pd.DataFrame(d)\n",
+    "print(df)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
